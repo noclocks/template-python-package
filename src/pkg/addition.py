44ac8b00131e96a69bb6c4716061d4d6fdf7da2d
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-04-29 20:27:29 -0400

    feat: add addition module src/pkg/addition.py

---
+++ src/pkg/addition.py
@@ -0,0 +1,23 @@
+"""Addition Module
+Module for simple addition (math) logic.
+"""
+
+def add_one(number: int) -> int:
+    """Add One
+    Args:
+        number (int): The number to add one to
+    Returns:
+        int
+    """
+    return number + 1
+
+
+def add_n(number: int, n: int) -> int:
+    """Add N
+    Args:
+        number (int): The number to add N to
+        n (int): The value to add to number.
+    Returns:
+        int
+    """
+    return number + n
